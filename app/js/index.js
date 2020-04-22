4c2b86140ea2112efa929148e7a469a99a364d32
Makalkin <alekseymakalkin00@gmail.com>
2020-04-22 04:05:27 +0300

    Version 1.2.0

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,29 @@
+// Кнопка для вызова меню
 var menuButton = document.querySelector(".nav .nav-touch");
+
+// Меню
+var menu = document.querySelector(".menu-bar");
+
+// false - меню скрыто    true - меню активно
 var menuHidden = false;
 
 
 
+
+// Функция описывает вызов меню. Так же меняет некоторые стили меню, что бы оно было адаптированно под маленькие экраны
 menuButton.onclick = function() {
     if (!menuHidden && window.innerWidth >= 715) {
-        document.querySelector(".menu-bar").style.width = "25%";
-        document.querySelector(".menu-bar").style.marginRight = "30px";
+        menu.style.width = "25%";
+        menu.style.marginRight = "30px";
 
         menuButton.style.backgroundColor = "#D8E1E9";
 
         menuHidden = true;
 
     } else if (!menuHidden && window.innerWidth < 715) {
-        document.querySelector(".menu-bar").style.float = "none";
-        document.querySelector(".menu-bar").style.width = "100%";
-        document.querySelector(".menu-bar").style.marginTop = "0px";
+        menu.style.float = "none";
+        menu.style.width = "100%";
+        menu.style.marginTop = "0px";
 
         menuButton.style.width = "100%";
         menuButton.style.backgroundColor = "#D8E1E9";
@@ -23,15 +31,37 @@ menuButton.onclick = function() {
         menuHidden = true;
 
     } else {
-        document.querySelector(".menu-bar").style.float = "left";
-        document.querySelector(".menu-bar").style.width = "0%";
-        document.querySelector(".menu-bar").style.marginTop = "-30px";
-        document.querySelector(".menu-bar").style.marginRight = "0px";
+        menu.style.float = "left";
+        menu.style.width = "0%";
+        menu.style.marginTop = "-30px";
+        menu.style.marginRight = "0px";
 
         menuButton.style.width = "70px";
         menuButton.style.backgroundColor = "#f2f2f2";
 
         menuHidden = false;
+
+    }
+}
+
+
+
+
+// Делаем так, что бы при изменении ширины экрана с уже открытым меню, оно так же подстраивалось под ширину экрана
+window.onresize = function() {
+    if (menuHidden && window.innerWidth >= 715) {
+        menu.style.float = "left";
+        menu.style.width = "25%";
+        menu.style.marginTop = "-30px";
+        menu.style.marginRight = "30px";
+        menuButton.style.width = "70px";
+
+    } else if (menuHidden && window.innerWidth < 715) {
+        menu.style.float = "none";
+        menu.style.width = "100%";
+        menu.style.marginTop = "0px";
+        menu.style.marginRight = "0px";
+        menuButton.style.width = "100%";
 
     }
 }
